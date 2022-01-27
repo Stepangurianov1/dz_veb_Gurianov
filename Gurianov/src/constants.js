8837738b5efbbae25a0a84f630481a87f14ab456
степан гурьянов <stepan.gurianov@gmail.com>
2022-01-28 01:03:21 +0300

    dz_Gurianov

---
+++ Gurianov/src/constants.js
@@ -0,0 +1,11 @@
+const firstName = 'повара'
+const secondName = 'свиток'
+const dz_name = 'Торговля шаурмой'
+const student = 'Гурьянов Степан'
+const first_form_first = 'имя повара'
+const second_form_first = 'название свитка'
+const second_form_second = ''
+const second_form_third = ''
+const domain = 'http://localhost:8000/api'
+
+export {firstName, secondName, domain, dz_name, student, first_form_first, second_form_second, second_form_third, second_form_first}
